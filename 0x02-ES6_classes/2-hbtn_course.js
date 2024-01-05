1099c6249e77368a379f7cee470fb8d5cb31213c
Fatimazahraker <fatimazahrakerdoun@gmail.com>
2024-01-05 22:41:06 +0000

    task

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,57 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    } else {
+      this._name = name;
+    }
+
+    if (typeof length !== 'number') {
+      throw new TypeError('length must be a number');
+    } else {
+      this._length = length;
+    }
+
+    if (!Array.isArray(students)) {
+      throw new TypeError('students must be an array');
+    } else {
+      this._students = students;
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set name(x) {
+    if (typeof x !== 'string') {
+      throw new TypeError('name must be a string');
+    } else {
+      this._name = x;
+    }
+  }
+
+  set length(x) {
+    if (typeof x !== 'number') {
+      throw new TypeError('length must be a number');
+    } else {
+      this._length = x;
+    }
+  }
+
+  set students(x) {
+    if (!Array.isArray(x)) {
+      throw new TypeError('students must be an array');
+    } else {
+      this._students = x;
+    }
+  }
+}
