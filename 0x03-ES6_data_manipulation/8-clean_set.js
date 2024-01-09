6dafbd651091893a874be020a14ce8b688a2bf7c
Fatimazahraker <fatimazahrakerdoun@gmail.com>
2024-01-09 23:23:59 +0000

    task

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,11 @@
+export default function(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+  const newString = [];
+  set.forEach((value) => {
+    if (value && value.startsWith(startString)) {
+      newString.push(value.slice(startString.length));
+    }
+  });
+  return newString.join('-');
+}
+
